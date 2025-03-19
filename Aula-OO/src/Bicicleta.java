6b82e8f28ec921661bdfb36e7c3c6a1182ca93c3
Daniel Augusto de Campos <daniel.campos@redisul.com.br>
2025-03-18 21:17:44 -0300

    Adicionado classe bicicleta

---
+++ Aula-OO/src/Bicicleta.java
@@ -0,0 +1,12 @@
+public class Bicicleta {
+    public String nome;
+
+    public String modelo;
+
+    public Integer marcha;
+
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Modelo: " + modelo);
+    }
+}
